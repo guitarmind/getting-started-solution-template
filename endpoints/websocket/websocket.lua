f7f1f787dce5d170cb7c29e5078b84673a4cc227
Nina Zhang <ninazhang@exosite.com>
2019-05-30 18:22:45 +0800

    test

--- endpoints/websocket/websocket.lua
+++ endpoints/websocket/websocket.lua
@@ -1,7 +1,7 @@
 --#ENDPOINT WEBSOCKET /ws
 -- Doc: http://docs.exosite.com/reference/services/websocket/#websocket_info
 if (websocketInfo.type == "open") then
-  websocketInfo.send("Welcome")
+  websocketInfo.send("Welcome11111")
 elseif (websocketInfo.type == "data") then
   -- Print the incoming message and close the connection
   print(websocketInfo.message)
